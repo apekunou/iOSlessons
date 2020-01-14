6166f8e8d586869e39a6d5df018edbf1848d38fe
Andrei Apekunou <andreiapekunou@Andreis-MacBook-Pro.local>
2020-01-14 19:41:51 +0300

    comment

---
+++ STSwiftSingleOne/STSwiftSingleOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STSwiftSingleOne
+//
+//  Created by Andrei Apekunou on 1/14/20.
+//  Copyright © 2020 Andrei Apekunou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
